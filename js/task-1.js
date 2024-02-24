bf87f48ce1d999d7af8ef58cb50f5b61c471600c
Nelli Diachkina <nelli.diachkina@gmail.com>
2024-02-24 16:50:01 +0200

    Initial commit

---
+++ js/task-1.js
@@ -0,0 +1,15 @@
+function isEnoughCapacity(products, containerSize) {
+  const productVolumes = Object.values(products);
+  let totalProductVolume = 0;
+
+  for (const productVolume of productVolumes) {
+    totalProductVolume += productVolume;
+  }
+
+  return totalProductVolume <= containerSize;
+}
+
+console.log(isEnoughCapacity({ apples: 2, grapes: 3, carrots: 1 }, 8)); // true
+console.log(isEnoughCapacity({ apples: 4, grapes: 6, lime: 16 }, 12)); // false
+console.log(isEnoughCapacity({ apples: 1, lime: 5, tomatoes: 3 }, 14)); // true
+console.log(isEnoughCapacity({ apples: 18, potatoes: 5, oranges: 2 }, 7)); // false
